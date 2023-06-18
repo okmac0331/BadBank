b90a337dda569b8895b711dd946011ea11ba01d2
okmac0331 <120515728+okmac0331@users.noreply.github.com>
2023-06-18 16:54:32 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { UserContext } from "./context";
-import NavBar from "./Navbar";
-import Home from "./Home";
-import CreateAccount from "./CreateAccount";
-import Login from "./Login";
-import Deposit from "./Deposit";
-import Withdraw from "./Withdraw";
-import AllData from "./Alldata";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Deposit from "./components/Deposit";
+import Withdraw from "./components/Withdraw";
+import UserAccount from "./components/UserAccount";
+import AllData from "./components/AllData";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <UserContext.Provider
-        value={{ users: [{ name: "abel", email: "abel@mit.edu", password: "secret", balance: 100 }] }}
-      >
-        <div className="container" style={{ padding: "20px" }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/createAccount/" element={<CreateAccount />} />
-            <Route path="/login/" element={<Login />} />
-            <Route path="/deposit/" element={<Deposit />} />
-            <Route path="/withdraw/" element={<Withdraw />} />
-            <Route path="/alldata/" element={<AllData />} />
-          </Routes>
-        </div>
-      </UserContext.Provider>
-    </Router>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="deposit" element={<Deposit />} />
+            <Route path="withdraw" element={<Withdraw />} />
+            <Route path="myAccount" element={<UserAccount />} />
+            <Route path="allData" element={<AllData />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <div>
+        <ToastContainer></ToastContainer>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { UserProvider } from "./context/UserContext";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <App />
+  <React.StrictMode>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </React.StrictMode>
 );
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-
-//   <React.StrictMode>
- 
-//     <App />
-  
-// </React.StrictMode>
-  
-// );
